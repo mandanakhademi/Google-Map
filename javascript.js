57a5aef36de20e54a6dad7e48ea5b6360ae19009
mandy kh@ <mandy@example.com>
2018-11-16 20:32:12 +0000

    S3- add Google Map Java API

--- javascript.js
+++ javascript.js
@@ -26,6 +26,26 @@ showListView = function(){
 
 }
 
+initialize = function() {
+  
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: new google.maps.LatLng(51.5141487121582, -0.10019999742507935),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  
+}
+ 
+loadScript = function() {
+let script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+    'callback=initialize';
+document.body.appendChild(script);
+}
+
+window.onload = loadScript;
+
 
 
 
